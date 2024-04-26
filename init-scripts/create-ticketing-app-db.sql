eb38e8658f21d662e0872606d2d554fd53771388
Viviana Pantazica <vivianapantazica@MacBook-Pro-Viviana.local>
2024-04-26 23:32:15 +0300

    added insert values script

--- init-scripts/create-ticketing-app-db.sql
+++ init-scripts/create-ticketing-app-db.sql
@@ -15,3 +15,30 @@ CREATE TABLE tickets (
     ticket_status VARCHAR(50) DEFAULT 'available' NOT NULL,
     price DECIMAL(10, 2) NOT NULL
 );
+
+INSERT INTO shows (show_name, date_and_time, place, show_description, inventory) 
+VALUES ('Keed Roast', '2024-05-30 18:00:00', 'Sala Luceafarul', 'Roast show', 2);
+
+INSERT INTO shows (show_name, date_and_time, place, show_description, inventory) 
+VALUES ('Barbierul din Sevilla', '2022-05-30 18:00:00', 'Opera Nationala', 'spectacol de opera', 1);
+
+INSERT INTO shows (show_name, date_and_time, place, show_description, inventory) 
+VALUES ('Show Horia Brenciu', '2024-07-12 18:00:00', 'Sala palatului', 'spectacol de muzica', 3);
+
+INSERT INTO tickets (show_id, costumer_email, place_number, ticket_status, price) 
+VALUES (1, NULL, 'A1', 'available', 250.00);
+
+INSERT INTO tickets (show_id, costumer_email, place_number, ticket_status, price) 
+VALUES (1, NULL, 'A2', 'available', 250.00);
+
+INSERT INTO tickets (show_id, costumer_email, place_number, ticket_status, price) 
+VALUES (2, NULL, '1', 'available', 550.00);
+
+INSERT INTO tickets (show_id, costumer_email, place_number, ticket_status, price) 
+VALUES (3, NULL, '1-K', 'available', 350.00);
+
+INSERT INTO tickets (show_id, costumer_email, place_number, ticket_status, price) 
+VALUES (3, NULL, '1-L', 'available', 350.00);
+
+INSERT INTO tickets (show_id, costumer_email, place_number, ticket_status, price) 
+VALUES (3, NULL, '1-M', 'available', 350.00);
